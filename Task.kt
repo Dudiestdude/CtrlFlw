f759c03450fe926857050f70654e963407d5ed2f
Dudiestdude <55890913+Dudiestdude@users.noreply.github.com>
2019-09-27 23:37:27 +0700

    Add files via upload

--- Task.kt
+++ Task.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Kotlin")
+    val listNumber = 1.rangeTo(30)
+
+    for (number in listNumber) {
+        // TODO 1
+        if (false) continue
+
+        // TODO 2
+        if (false) break
+
+        // TODO 3
+        val result = 0
+        println("result is $result")
+    }
 }
